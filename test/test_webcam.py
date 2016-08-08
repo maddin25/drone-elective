5847fc70af8ab950f22a91ac2b809bbdd6abde7c
Martin Patz <mailto@martin-patz.de>
2016-08-08 22:39:05 +0200

    the button "v" switches between front and down facing camera

--- test/test_webcam.py
+++ test/test_webcam.py
@@ -4,7 +4,7 @@ import numpy as np
 import asci_keys as keys
 
 drone = lib_drone.ARDrone(True)
-
+view_front_camera = True
 drone_feed_window = "Drone feed"
 delay_time = int(40)  # [ms]
 image_shape = drone.image_shape  # (720, 1280, 3)
@@ -19,7 +19,10 @@ while True:
         break
     elif key in [keys.h, keys.H]:
         print "Hello, thanks for greeting me."
-
+    elif key in [keys.v, keys.V]:
+        view_front_camera = not view_front_camera
+        print "Showing front-facing camera: " + str(view_front_camera)
+        drone.set_camera_view(view_front_camera)
     img = drone.get_image()
     img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
     bat = drone.navdata.get(0, dict()).get('battery', 0)
