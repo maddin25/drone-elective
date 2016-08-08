3027af1f81ffa663183c581092fb223e65daa45c
Martin Patz <mailto@martin-patz.de>
2016-08-08 11:50:46 +0200

    initial upload

---
+++ test_webcam.py
@@ -0,0 +1,14 @@
+import cv2
+
+cam = cv2.VideoCapture(0)
+running= True
+while running:
+   running, frame=cam.read()
+   if running:
+       cv2.imshow('frame',frame)
+   if cv2.waitKey(1) & 0xFF==27:
+       running= False
+   else:
+       print 'error reading video feed'
+cam.release()
+cv2.destroyAllWindows()
