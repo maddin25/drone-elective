8b85231dcb31df4a80069cfc58d75ceed7ec4d61
Martin Patz <mailto@martin-patz.de>
2016-08-08 22:32:39 +0200

    showing camera stream in real time (25 fps / 40 mHz)

--- test/test_webcam.py
+++ test/test_webcam.py
@@ -1,26 +1,28 @@
 import cv2 as cv
 import libardrone.libardrone as lib_drone
 import numpy as np
-
+import asci_keys as keys
 
 drone = lib_drone.ARDrone(True)
 
 drone_feed_window = "Drone feed"
-image_shape = drone.image_shape
-# image_shape = (720, 1280, 3)
-img = np.zeros([image_shape[0], image_shape[1], image_shape[2]])
-img[:,:,0] = np.ones([image_shape[0], image_shape[1]]) * 200 / 255.0
-img[:,:,1] = np.ones([image_shape[0], image_shape[1]]) * 200 / 255.0
-img[:,:,2] = np.ones([image_shape[0], image_shape[1]]) * 200 / 255.0
+delay_time = int(40)  # [ms]
+image_shape = drone.image_shape  # (720, 1280, 3)
+img = np.ones([image_shape[0], image_shape[1], image_shape[2]]) * 200 / 255.0
 
-
-i = 10
-while i > 0:
+while True:
     cv.imshow(drone_feed_window, img)
+
+    # Select action
+    key = cv.waitKey(delay_time)
+    if key is keys.esc:
+        break
+    elif key in [keys.h, keys.H]:
+        print "Hello, thanks for greeting me."
+
     img = drone.get_image()
     img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
     bat = drone.navdata.get(0, dict()).get('battery', 0)
-    cv.waitKey()
-    i -= 1
+
 
 drone.halt()
