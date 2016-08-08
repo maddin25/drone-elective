664c46daa24bda8c05e9d4cc562647fea0fb1adb
Martin Patz <mailto@martin-patz.de>
2016-08-08 21:53:03 +0200

    first working version of retrieving images from the drone

--- test/test_webcam.py
+++ test/test_webcam.py
@@ -1,14 +1,26 @@
-import cv2
+import cv2 as cv
+import libardrone.libardrone as lib_drone
+import numpy as np
 
-cam = cv2.VideoCapture(0)
-running = True
-while running:
-   running, frame=cam.read()
-   if running:
-       cv2.imshow('frame',frame)
-   if cv2.waitKey(1) & 0xFF == 27: # ESC key
-       running = False
-   else:
-       print 'error reading video feed'
-cam.release()
-cv2.destroyAllWindows()
+
+drone = lib_drone.ARDrone(True)
+
+drone_feed_window = "Drone feed"
+image_shape = drone.image_shape
+# image_shape = (720, 1280, 3)
+img = np.zeros([image_shape[0], image_shape[1], image_shape[2]])
+img[:,:,0] = np.ones([image_shape[0], image_shape[1]]) * 200 / 255.0
+img[:,:,1] = np.ones([image_shape[0], image_shape[1]]) * 200 / 255.0
+img[:,:,2] = np.ones([image_shape[0], image_shape[1]]) * 200 / 255.0
+
+
+i = 10
+while i > 0:
+    cv.imshow(drone_feed_window, img)
+    img = drone.get_image()
+    img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+    bat = drone.navdata.get(0, dict()).get('battery', 0)
+    cv.waitKey()
+    i -= 1
+
+drone.halt()
