37abcb50d0ba3d75b11d119a5f7616b2f0770d14
R047038\Daisy <daisyufjf@gmail.com>
2016-08-09 15:48:52 +0200

    Changes in manual control: left, right, land, take off...

--- test/test_manual_controls.py
+++ test/test_manual_controls.py
@@ -2,11 +2,15 @@ import cv2 as cv
 import libardrone.libardrone as lib_drone
 import numpy as np
 import asci_keys as keys
+import time
 
 drone = lib_drone.ARDrone(True)
 delay_time = int(40)  # [ms]
 
 running = True
+
+flying = False
+
 while running:
     key = cv.waitKey(delay_time)
 
@@ -14,6 +18,29 @@ while running:
         drone.move_left()
     elif key in [keys.d, keys.D]:
         drone.move_right()
-        
+    elif key in [keys.w, keys.W]:
+        drone.move_forward()
+    elif key in [keys.s, keys.S]:
+        drone.move_backward()
+    elif key in [keys.i, keys.I]:
+        drone.move_up()
+    elif key in [keys.m, keys.M]:
+        drone.move_down()
+    elif key in [keys.j, keys.J]:
+        drone.turn_left()
+    elif key in [keys.k, keys.K]:
+        drone.turn_right()
+    elif key in [keys.space]:
+        if not flying:
+            drone.takeoff()
+        else:
+            drone.land()
+    elif key in [keys.backspace]:
+        if not flying:
+            drone.halt()
+        else:
+            drone.land()
+            time.sleep(8)
+            drone.halt()
 
 drone.halt()
