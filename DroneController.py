77699856505808f30c6c2d6b7106afcd2a28dad6
Martin Patz <mailto@martin-patz.de>
2016-08-10 09:32:39 +0200

    added new method handle_key_stroke

--- DroneController.py
+++ DroneController.py
@@ -21,5 +21,40 @@ class DroneController:
         assert new_cycle_time > 0
         self.cycle_time = new_cycle_time
 
-
+    def handle_key_stroke(self, key):
+        if key in [keys.a, keys.A]:
+            self.drone.move_left()
+        elif key in [keys.d, keys.D]:
+            self.drone.move_right()
+        elif key in [keys.w, keys.W]:
+            self.drone.move_forward()
+        elif key in [keys.s, keys.S]:
+            self.drone.move_backward()
+        elif key in [keys.i, keys.I]:
+            self.drone.move_up()
+        elif key in [keys.m, keys.M]:
+            self.drone.move_down()
+        elif key in [keys.j, keys.J]:
+            self.drone.turn_left()
+        elif key in [keys.k, keys.K]:
+            self.drone.turn_right()
+        elif key in [keys.space]:
+            if not self.flying:
+                self.drone.takeoff()
+                self.flying = True
+            else:
+                self.drone.land()
+                self.flying = False
+        elif key in [keys.backspace]:
+            if not self.flying:
+                self.drone.halt()
+            else:
+                self.drone.land()
+                self.flying = False
+                time.sleep(8)
+                self.drone.halt()
+            return True
+        elif key is keys.esc:
+            return True
+        return False
 
